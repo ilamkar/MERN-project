cdf8beff3684069b1788cb30a3b24fd1daefbb56
ilamkar <illum_dae@yahoo.com>
2020-02-19 19:39:12 -0600

    adding components and styling them

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import './App.css';
-import {BrowserRouter as Router, Route, Redirect,Switch} from 'react-router-dom';
-import Users from './user/components/pages/Users';
-import NewPlace from './places/components/pages/NewPlace';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from 'react-router-dom';
 
-const App = ()=> {
-  
+import Users from './user/pages/Users';
+import NewPlace from './places/pages/NewPlace';
+
+const App = () => {
   return (
-   <Router>
-     <Switch>
-    <Route path="/users" exact>
-    <Users/>
-    </Route>
-
-    <Route path="/places/new" exact>
-    <NewPlace/>
-    </Route>
-
-    <Redirect to="/users"/>
-    </Switch>
-  </Router>
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Users />
+        </Route>
+        <Route path="/places/new" exact>
+          <NewPlace />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    </Router>
   );
-} 
+};
 
 export default App;
