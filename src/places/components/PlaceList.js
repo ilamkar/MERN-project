ca2f8b95120ad57f5cdf4c1571d66fcfa9cd2cde
ilamkar <illum_dae@yahoo.com>
2020-02-21 21:13:36 -0600

    rendering map on app  using google map

--- src/places/components/PlaceList.js
+++ src/places/components/PlaceList.js
@@ -2,9 +2,7 @@ import React from 'react';
 
 import Card from '../../shared/components/UIElements/Card';
 import PlaceItem from './PlaceItem';
-import Button from '../../shared/components/FormElements/Button';
 import './PlaceList.css';
-
 
 const PlaceList = props => {
   if (props.items.length === 0) {
@@ -12,7 +10,7 @@ const PlaceList = props => {
       <div className="place-list center">
         <Card>
           <h2>No places found. Maybe create one?</h2>
-          <Button>Share Place</Button>
+          <button>Share Place</button>
         </Card>
       </div>
     );
